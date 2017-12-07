3ea5322489401a11c65413455702cd4f58fa4cb6
Eric Campbell <ericcampbell59@gmail.com>
2017-12-06 18:15:11 -0800

    set default args for the gdax client and bump version

--- lib/tokn_exchange_client/gdax_client.rb
+++ lib/tokn_exchange_client/gdax_client.rb
@@ -4,7 +4,7 @@ require 'base64'
 # require 'json'
 
 class ToknExchangeClient::GDAXClient
-  def initialize(key, secret, passphrase)
+  def initialize(key=nil, secret=nil, passphrase=nil)
     @api_key = key
     @secret = secret
     @passphrase = passphrase
@@ -27,7 +27,6 @@ class ToknExchangeClient::GDAXClient
   ### Accounts
   ###
   def list_accounts
-    return unless @api_key
     resp = HTTP.get(@base_path + "accounts")
     return JSON.parse(resp)
   end
@@ -36,37 +35,31 @@ class ToknExchangeClient::GDAXClient
   ### Products
   ###
   def get_products
-    return unless @api_key
     resp = HTTP.get(@base_path + "products")
     return JSON.parse(resp)
   end
 
   def get_product_order_book(product_id)
-    return unless @api_key
     resp = HTTP.get(@base_path + "products/#{product_id}/book")
     return JSON.parse(resp)
   end
 
   def get_product_ticker(product_id)
-    return unless @api_key
     resp = HTTP.get(@base_path + "products/#{product_id}/ticker")
     return JSON.parse(resp)
   end
 
   def get_product_trades(product_id)
-    return unless @api_key
     resp = HTTP.get(@base_path + "products/#{product_id}/trades")
     return JSON.parse(resp)
   end
 
   def get_product_historic_rates(product_id)
-    return unless @api_key
     resp = HTTP.get(@base_path + "products/#{product_id}/candles")
     return JSON.parse(resp)
   end
 
   def get_product_stats(product_id)
-    return unless @api_key
     resp = HTTP.get(@base_path + "products/#{product_id}/stats")
     return JSON.parse(resp)
   end
@@ -75,7 +68,6 @@ class ToknExchangeClient::GDAXClient
   ### Currencies
   ###
   def get_currencies
-    return unless @api_key
     resp = HTTP.get(@base_path + "currencies")
     return JSON.parse(resp)
   end
@@ -84,7 +76,6 @@ class ToknExchangeClient::GDAXClient
   ### Time
   ###
   def get_time
-    return unless @api_key
     resp = HTTP.get(@base_path + "time")
     return JSON.parse(resp)
   end
